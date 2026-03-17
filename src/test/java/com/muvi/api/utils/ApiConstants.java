bc3c40326b6cb9dfe5f16d9156668eda7283d646
Suvendu Shekhar Samal <134824290+suvendushekharsamal@users.noreply.github.com>
2026-03-17 21:33:43 +0530

    Update POSTS_ENDPOINT to new products/add URI

--- src/test/java/com/muvi/api/utils/ApiConstants.java
+++ src/test/java/com/muvi/api/utils/ApiConstants.java
@@ -4,5 +4,5 @@ public class ApiConstants {
 
 	    public static final String BASE_URI = "https://dummyjson.com";
 	    public static final String USERS_ENDPOINT = "/products";
-	    public static final String POSTS_ENDPOINT = "/posts";
+	    public static final String POSTS_ENDPOINT = "/products/add";
 	}
