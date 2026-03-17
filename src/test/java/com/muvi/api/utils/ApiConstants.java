06f98ba4167a1c87ea79e1d27180cfcea38ba5ea
Suvendu Shekhar Samal <134824290+suvendushekharsamal@users.noreply.github.com>
2026-03-17 21:28:21 +0530

    Change USERS_ENDPOINT to point to products

--- src/test/java/com/muvi/api/utils/ApiConstants.java
+++ src/test/java/com/muvi/api/utils/ApiConstants.java
@@ -3,6 +3,6 @@ package com.muvi.api.utils;
 public class ApiConstants {
 
 	    public static final String BASE_URI = "https://dummyjson.com";
-	    public static final String USERS_ENDPOINT = "/users";
+	    public static final String USERS_ENDPOINT = "/products";
 	    public static final String POSTS_ENDPOINT = "/posts";
 	}
