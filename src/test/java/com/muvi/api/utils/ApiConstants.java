f2f7021a336d09f9f402f69aab1dea38abb565be
Suvendu Shekhar Samal <134824290+suvendushekharsamal@users.noreply.github.com>
2026-03-17 21:22:30 +0530

    Fix BASE_URI declaration in ApiConstants

--- src/test/java/com/muvi/api/utils/ApiConstants.java
+++ src/test/java/com/muvi/api/utils/ApiConstants.java
@@ -2,7 +2,7 @@ package com.muvi.api.utils;
 
 public class ApiConstants {
 
-	    public static final String BASE_URI = "https://dummyjson.com"";
+	    public static final String BASE_URI = "https://dummyjson.com";
 	    public static final String USERS_ENDPOINT = "/users";
 	    public static final String POSTS_ENDPOINT = "/posts";
 	}
