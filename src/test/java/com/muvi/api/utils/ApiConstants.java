bba0d1e4f1f96fbe20b5260d49dfc62e20016a9a
Suvendu Shekhar Samal <134824290+suvendushekharsamal@users.noreply.github.com>
2026-03-19 14:14:32 +0530

    Update first changes README.md

--- src/test/java/com/muvi/api/utils/ApiConstants.java
+++ src/test/java/com/muvi/api/utils/ApiConstants.java
@@ -3,6 +3,6 @@ package com.muvi.api.utils;
 public class ApiConstants {
 
 	    public static final String BASE_URI = "https://dummyjson.com";
-	    public static final String USERS_ENDPOINT = "/products";
+	    public static final String GET_PRODUCT_ENDPOINT = "/products";
 	    public static final String POSTS_ENDPOINT = "/products/add";
 	}
