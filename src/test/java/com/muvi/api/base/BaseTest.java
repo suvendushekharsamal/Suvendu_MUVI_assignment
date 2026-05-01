6c03915e2a092538d2a09e0d69b83c733d58b69a
Suvendu Shekhar Samal <134824290+suvendushekharsamal@users.noreply.github.com>
2026-05-01 17:27:35 +0530

    Refactor setup method in BaseTest class
    Removed unnecessary comment and fixed formatting.

--- src/test/java/com/muvi/api/base/BaseTest.java
+++ src/test/java/com/muvi/api/base/BaseTest.java
@@ -8,11 +8,8 @@ import com.muvi.api.utils.ApiConstants;
 public class BaseTest {
     @BeforeClass
     public void setup() {
-<<<<<<< HEAD
+
         RestAssured.baseURI = "ApiConstants.BASE_URI"; 
-=======
-    	
-        RestAssured.baseURI = ApiConstants.BASE_URI; // ✅ use HTTPS
->>>>>>> 283ea63 (Your commit message)
+
     }
 }
